0542a448eef3ed8856ff903eaad9e6a507e22d85
pauline nemchak <doublewebstandards@gmail.com>
2024-08-11 03:30:44 +0100

    changed spelling in rss feed page

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 
 export const get = () => rss({
     title: 'Pauline Nemchak | Blog',
-    description: 'Blog about frontend, DSP and the other stuff.',
+    description: 'Blog about front-end, DSP and the other stuff.',
     site: 'https://paulinenemchak.dev/',
     items: import.meta.glob('./**/*.md'),
     customData: `<language>en-uk</language>`,
