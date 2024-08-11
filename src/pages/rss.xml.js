ab5596133939e1c01fb022cf35973bb54f1c1d52
pauline nemchak <doublewebstandards@gmail.com>
2024-08-11 17:18:38 +0100

    updated rss collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,19 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export const get = () => rss({
-    title: 'Pauline Nemchak | Blog',
-    description: 'Blog about front-end, DSP and the other stuff.',
-    site: 'https://paulinenemchak.dev/',
-    items: import.meta.glob('./**/*.md'),
-    customData: `<language>en-uk</language>`,
-});
+export const GET = async () => {
+    const posts = await getCollection('posts');
+
+    return rss({
+        title: 'Pauline Nemchak | Blog',
+        description: 'Blog about front-end, DSP and the other stuff.',
+        site: 'https://paulinenemchak.dev/',
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            customData: post.data.customData,
+            link: `/posts/${post.slug}/`,
+        })),
+    });
+};
